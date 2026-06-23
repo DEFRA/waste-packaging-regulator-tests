a7fd1fa2e5873863673d77b107feb87c4d7ce24c
Daniel Ashton <daniel.ashton@dashkat.uk>
2026-06-23 20:02:35 +0100

    AMCR-290 - Add page object link for first item in pending
    Move login to page class instead for reuse with list page

--- test/page-objects/page.js
+++ test/page-objects/page.js
@@ -10,6 +10,13 @@ class Page {
   async open(path) {
     await this.page.goto(path)
   }
+
+  async login(email, password) {
+    await this.page.getByLabel('Email address').fill(email)
+    await this.page.getByLabel('Password').fill(password)
+    await this.page.getByRole('button', { name: 'Sign in' }).click()
+    await this.page.waitForURL((url) => !url.hostname.includes('b2clogin'))
+  }
 }
 
 export { Page }
