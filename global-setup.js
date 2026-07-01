cdaebaef035515c222a416b94f550e2599d1a319
Jeyakumar Rathinavelu <5055383+Jey20@users.noreply.github.com>
2026-07-01 15:26:23 +0100

    attempt to fix the proxy - timeout issue

--- global-setup.js
+++ global-setup.js
@@ -1,7 +1,4 @@
-import { isSecurity } from './utils/profile.js'
-import { startSpiderScan } from './utils/zap/zap.js'
-
 export default async function globalSetup() {
-  if (!isSecurity) return
-  await startSpiderScan(process.env.baseURL)
+  // if (!isSecurity) return
+  // await startSpiderScan(process.env.baseURL)
 }
