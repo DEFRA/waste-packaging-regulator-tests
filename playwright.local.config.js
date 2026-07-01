6aa6d443c27ac7f01e0c79009d8938194ab8bac0
Jeyakumar Rathinavelu <5055383+Jey20@users.noreply.github.com>
2026-07-01 18:58:45 +0100

    enable run against local instance

--- playwright.local.config.js
+++ playwright.local.config.js
@@ -1,6 +1,14 @@
-import 'dotenv/config'
-import { defineConfig } from '@playwright/test'
+import dotenv from 'dotenv'
+import { defineConfig, devices } from '@playwright/test'
 import baseConfig from './playwright.config.js'
+
+dotenv.config({ path: '.env.local', override: true })
+
+const nationId = process.env.NATION_ID ?? 'EN'
+const authFile = `playwright/.auth/nation${nationId}.json`
+
+const baseURL = process.env.baseURL
+const baseURLCompliance = process.env.baseURLCompliance
 
 export default defineConfig({
   ...baseConfig,
@@ -8,8 +16,18 @@ export default defineConfig({
   retries: 0,
   use: {
     ...baseConfig.use,
-    baseURL: process.env.EPR_BASE_URL || 'https://localhost:7154',
+    baseURL,
+    baseURLCompliance,
     trace: 'on',
     video: 'on'
-  }
+  },
+  projects: [
+    {
+      name: 'Regulator Dashboard Functional Tests',
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: authFile
+      }
+    }
+  ]
 })
