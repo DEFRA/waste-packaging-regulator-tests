b17717751e14db78da3ee9fc40494e4db8356e41
Jeyakumar Rathinavelu <5055383+Jey20@users.noreply.github.com>
2026-07-14 12:24:47 +0100

    Publis ZAP Log in AWS S3 bucket

--- bin/publish-tests.sh
+++ bin/publish-tests.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 DIRECTORY="$PWD/allure-report"
+ZAP_LOG="/tmp/zap-${RUN_ID:-local}.log"
 
 echo "Publishing test results to S3"
 
 if [ -n "$RESULTS_OUTPUT_S3_PATH" ]; then
    if [ -d "$DIRECTORY" ]; then
       aws s3 cp --quiet "$DIRECTORY" "$RESULTS_OUTPUT_S3_PATH" --recursive
+      aws s3 cp --quiet "$ZAP_LOG" "$RESULTS_OUTPUT_S3_PATH/ZAP_LOG"
+      
       echo "Test results published to $RESULTS_OUTPUT_S3_PATH"
    else
       echo "$DIRECTORY is not found"
