f2e293d8180b91e089371005a76166e0d5ffb3f6
Geoff Kaile <geoff@recipe4.life>
2026-09-22 10:46:08 +0100

    AMCR-492: Updated url pattern to allow for stateful return to list from details page

--- test/specs/certs_statements_of_compliance_not_submitted_detail.spec.js
+++ test/specs/certs_statements_of_compliance_not_submitted_detail.spec.js
@@ -3,7 +3,7 @@ import { CertificatesDetailPage } from '../page-objects/certificates.detail.page
 import { CertificatesPage } from '../page-objects/certificates.page.js'
 
 const notSubmittedDetailUrlPattern =
-  /\/certificates-of-compliance\/[0-9a-f-]+\?obligationYear=\d{4}$/
+  /\/certificates-of-compliance\/[0-9a-f-]+\?obligationYear=\d{4}(&.*)?$/
 
 async function openNotSubmittedDetail(page, organisationType, testInfo) {
   const certificatesPage = new CertificatesPage(page)
